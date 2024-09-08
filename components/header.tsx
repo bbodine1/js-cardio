61354ec96fdb3f649b00cb4891e10d96a4ec4985
Brad <bbodine1@gmail.com>
2024-09-08 14:41:35 -0500

    add button for github link

--- components/header.tsx
+++ components/header.tsx
@@ -1,32 +1,43 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
-import { Moon, Sun } from 'lucide-react'
+import { Moon, Sun, Github } from 'lucide-react'
 
 interface HeaderProps {
-  theme: 'light' | 'dark'
-  toggleTheme: () => void
+	theme: 'light' | 'dark'
+	toggleTheme: () => void
 }
 
 export const Header: React.FC<HeaderProps> = ({ theme, toggleTheme }) => {
-  return (
-    <header>
-      <div className="flex justify-between items-center mb-4">
-        <h1 className="text-2xl font-bold">JavaScript Cardio</h1>
+	const openGitHubRepo = () => {
+		window.open('https://github.com/bbodine1/js-cardio/stargazers', '_blank')
+	}
 
-        <Button
-          onClick={toggleTheme}
-          variant={theme === 'dark' ? 'ghost' : 'default'}
-          size="icon"
-          aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} theme`}
-          data-testid="theme-toggle-btn"
-        >
-          {theme === 'light' ? (
-            <Moon className="h-[1.2rem] w-[1.2rem]" />
-          ) : (
-            <Sun className="h-[1.2rem] w-[1.2rem]" />
-          )}
-        </Button>
-      </div>
-    </header>
-  )
+	return (
+		<header>
+			<div className="flex justify-between items-center mb-4">
+				<h1 className="text-2xl font-bold">JavaScript Cardio</h1>
+
+				<div className="flex gap-2">
+					<Button
+						onClick={openGitHubRepo}
+						variant={theme === 'dark' ? 'ghost' : 'default'}
+						size="icon"
+						aria-label="Open GitHub repository"
+					>
+						<Github className="h-[1.2rem] w-[1.2rem]" />
+					</Button>
+
+					<Button
+						onClick={toggleTheme}
+						variant={theme === 'dark' ? 'ghost' : 'default'}
+						size="icon"
+						aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} theme`}
+						data-testid="theme-toggle-btn"
+					>
+						{theme === 'light' ? <Moon className="h-[1.2rem] w-[1.2rem]" /> : <Sun className="h-[1.2rem] w-[1.2rem]" />}
+					</Button>
+				</div>
+			</div>
+		</header>
+	)
 }
