c59f6d8f467763abf113dbea2ea172b3a5d56fd3
Brad Bodine <bbodine1@gmail.com>
2024-09-09 14:35:23 -0500

    fix eslint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import pluginReact from 'eslint-plugin-react'
-import pluginReactHooks from 'eslint-plugin-react-hooks'
-import pluginJsxA11y from 'eslint-plugin-jsx-a11y'
+import js from '@eslint/js'
+import * as tseslint from 'typescript-eslint'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y'
 import nextPlugin from '@next/eslint-plugin-next'
 
 export default [
@@ -16,14 +16,21 @@ export default [
 			},
 		},
 	},
-	pluginJs.configs.recommended,
+	js.configs.recommended,
 	...tseslint.configs.recommended,
-	pluginReact.configs.recommended,
-	pluginReactHooks.configs.recommended,
-	pluginJsxA11y.configs.recommended,
-	nextPlugin.configs.recommended,
 	{
+		files: ['**/*.{js,jsx,ts,tsx}'],
+		plugins: {
+			react: reactPlugin,
+			'react-hooks': reactHooksPlugin,
+			'jsx-a11y': jsxA11yPlugin,
+			'@next/next': nextPlugin,
+		},
 		rules: {
+			...reactPlugin.configs.recommended.rules,
+			...reactHooksPlugin.configs.recommended.rules,
+			...jsxA11yPlugin.configs.recommended.rules,
+			...nextPlugin.configs.recommended.rules,
 			'react/react-in-jsx-scope': 'off', // Not needed in Next.js
 			'react/prop-types': 'off', // If using TypeScript
 		},
