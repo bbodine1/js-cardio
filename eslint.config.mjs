53d3cb6e57f52e40f9c26188e07a2d0e3209bc77
Brad Bodine <bbodine1@gmail.com>
2024-09-09 13:43:14 -0500

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y'
+import nextPlugin from '@next/eslint-plugin-next'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+	{
+		files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.recommended,
+	pluginReactHooks.configs.recommended,
+	pluginJsxA11y.configs.recommended,
+	nextPlugin.configs.recommended,
+	{
+		rules: {
+			'react/react-in-jsx-scope': 'off', // Not needed in Next.js
+			'react/prop-types': 'off', // If using TypeScript
+		},
+	},
+]
